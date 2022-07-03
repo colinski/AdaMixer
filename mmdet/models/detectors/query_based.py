512bb1fafc3a71d6a2a762dd2361757a6fb61184
Colin <csamplawski@cs.umass.edu>
2022-07-03 16:20:51 -0400

    added code to collect output and train models with decoder only

--- mmdet/models/detectors/query_based.py
+++ mmdet/models/detectors/query_based.py
@@ -9,3 +9,65 @@ class QueryBased(SparseRCNN):
     We hack and build our model into Sparse RCNN framework implementation
     in mmdetection.
     '''
+
+    def __init__(self, *args, **kwargs):
+        self.freeze_backbone = False
+        if 'freeze_backbone' in kwargs:
+            self.freeze_backbone = kwargs['freeze_backbone']
+            del kwargs['freeze_backbone']
+        super().__init__(*args, **kwargs)
+
+    def forward_train(self,
+                      img,
+                      img_metas,
+                      gt_bboxes,
+                      gt_labels,
+                      gt_bboxes_ignore=None,
+                      gt_masks=None,
+                      proposals=None,
+                      **kwargs):
+        """Forward function of SparseR-CNN in train stage.
+
+        Args:
+            img (Tensor): of shape (N, C, H, W) encoding input images.
+                Typically these should be mean centered and std scaled.
+            img_metas (list[dict]): list of image info dict where each dict
+                has: 'img_shape', 'scale_factor', 'flip', and may also contain
+                'filename', 'ori_shape', 'pad_shape', and 'img_norm_cfg'.
+                For details on the values of these keys see
+                :class:`mmdet.datasets.pipelines.Collect`.
+            gt_bboxes (list[Tensor]): Ground truth bboxes for each image with
+                shape (num_gts, 4) in [tl_x, tl_y, br_x, br_y] format.
+            gt_labels (list[Tensor]): class indices corresponding to each box
+            gt_bboxes_ignore (None | list[Tensor): specify which bounding
+                boxes can be ignored when computing the loss.
+            gt_masks (List[Tensor], optional) : Segmentation masks for
+                each box. But we don't support it in this architecture.
+            proposals (List[Tensor], optional): override rpn proposals with
+                custom proposals. Use when `with_rpn` is False.
+
+        Returns:
+            dict[str, Tensor]: a dictionary of loss components
+        """
+
+        assert proposals is None, 'Sparse R-CNN does not support' \
+                                  ' external proposals'
+        assert gt_masks is None, 'Sparse R-CNN does not instance segmentation'
+
+        x = self.extract_feat(img)
+        if self.freeze_backbone:
+            x = tuple([y.detach() for y in x])
+        proposal_boxes, proposal_features, imgs_whwh = \
+            self.rpn_head.forward_train(x, img_metas)
+        roi_losses = self.roi_head.forward_train(
+            x,
+            proposal_boxes,
+            proposal_features,
+            img_metas,
+            gt_bboxes,
+            gt_labels,
+            gt_bboxes_ignore=gt_bboxes_ignore,
+            gt_masks=gt_masks,
+            imgs_whwh=imgs_whwh)
+        return roi_losses
+
---
+++ tools/collect_output.py
@@ -0,0 +1,85 @@
+import numpy as np
+import torch
+from mmdet.apis import init_detector
+from mmdet.datasets import build_dataset, build_dataloader
+from mmdet.core import bbox_cxcywh_to_xyxy, bbox_xyxy_to_cxcywh
+from tqdm import tqdm, trange#progress bar
+import pickle
+import sys
+
+#init model and dataloader
+config = sys.argv[1]
+checkpoint = sys.argv[2]
+pkl_fname = sys.argv[3]
+model = init_detector(config, checkpoint).cuda().eval()
+dataset = build_dataset(model.cfg.data.val)
+# dataloader = build_dataloader(dataset, samples_per_gpu=1, workers_per_gpu=2, dist=False, shuffle=False)
+
+output = []
+
+for idx in trange(len(dataset)):
+    sample = dataset[idx]
+    gt = dataset.get_ann_info(idx)
+    img = sample['img'][0].cuda().unsqueeze(0)
+    img_metas = [sample['img_metas'][0].data]
+    img_metas[0]['batch_input_shape'] = (img.shape[2], img.shape[3])
+    result = {k: v for k, v in img_metas[0].items()}
+
+    with torch.no_grad():
+        x = model.extract_feat(img)
+        # proposal_boxes, proposal_features, imgs_whwh = \
+            # model.rpn_head.forward_train(x, img_metas)
+        # proposal_boxes, proposal_features, imgs_whwh = \
+            # model.rpn_head.simple_test_rpn(x, img_metas)
+
+        query_xyzr, query_content, imgs_whwh = \
+            model.rpn_head.simple_test_rpn(x, img_metas)
+
+
+        num_imgs = len(img_metas)
+        num_queries = query_xyzr.size(1)
+        imgs_whwh = imgs_whwh.repeat(1, num_queries, 1)
+        all_stage_bbox_results = []
+        all_stage_loss = {}
+        
+        all_cls_score, all_bbox_pred = [], []
+        for stage in range(model.roi_head.num_stages):
+            bbox_results = model.roi_head._bbox_forward(stage, x, query_xyzr, query_content,
+                                              img_metas)
+            
+            #for future layers
+            query_xyzr = bbox_results['query_xyzr']#.detach()
+            query_content = bbox_results['query_content']
+
+            # all_stage_bbox_results.append(bbox_results)
+            # if gt_bboxes_ignore is None:
+                # gt_bboxes_ignore = [None for _ in range(num_imgs)]
+            # cls_pred_list = bbox_results['detach_cls_score_list']
+            # bboxes_list = bbox_results['detach_bboxes_list']
+
+            # query_xyzr = bbox_results['query_xyzr'].detach()
+            # query_content = bbox_results['query_content']
+
+            cls_score, bbox_pred = [], []          
+            for i in range(num_imgs):
+                cls_score.append(bbox_results['cls_score'][i])
+                norm_bbox = bbox_xyxy_to_cxcywh(bbox_results['decode_bbox_pred'][i] / imgs_whwh[i])
+                bbox_pred.append(norm_bbox)
+            all_cls_score.append(torch.stack(cls_score))
+            all_bbox_pred.append(torch.stack(bbox_pred))
+        all_cls_score = torch.stack(all_cls_score).squeeze()
+        all_cls_score = torch.softmax(all_cls_score, dim=-1)
+        all_bbox_pred = torch.stack(all_bbox_pred).squeeze()
+        
+    #collect output
+    # result['query_embeds'] = query_embeds.cpu().numpy()
+    result['bbox_preds'] = all_bbox_pred.cpu().numpy()
+    result['cls_probs'] = all_cls_score.cpu().numpy()
+
+    result['gt_bboxes'] = gt['bboxes']
+    result['gt_labels'] = gt['labels']
+    output.append(result)
+    
+#save to pickle file
+with open(pkl_fname, 'wb') as f:
+    pickle.dump(output, f)
